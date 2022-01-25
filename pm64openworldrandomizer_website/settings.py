2c6388ac896bd5d7ba194a53a4ee41b16d8774b9
Pronyo <franktb93@gmail.com>
2022-01-24 21:08:26 -0500

    Add CORS module so the web app can communicate with server

--- pm64openworldrandomizer_website/settings.py
+++ pm64openworldrandomizer_website/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ALLOWED_ORIGINS  = (
+    'http://localhost:4200', # Allowed host, if CORS_ORIGIN_ALLOW_ALL is False
+)
+
 
 # Application definition
 
@@ -37,14 +43,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'corsheaders',
     'rest_framework',
     'randomizer',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',        
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
