753732f8d9acf944267c69f5cbe6ee6bee62825c
Pronyo <franktb93@gmail.com>
2022-05-23 17:50:24 -0400

    Revert changes in seed schema; Allow setting cosmetics both at creation and post

--- models/database/seed.py
+++ models/database/seed.py
@@ -17,13 +17,16 @@ class Seed:
         NoSaveBlocks: bool, NoHeartBlocks: bool, FlowerGateOpen: bool, BlueHouseOpen: bool, ToyboxOpen: bool, WhaleOpen: bool, ShuffleChapterDifficulty: bool,\
         RandomFormations: bool, ShuffleItems: bool, IncludeCoins: bool, IncludeShops: bool, IncludePanels: bool, IncludeFavorsMode: int, IncludeLettersMode: int, KeyitemsOutsideDungeon: bool,\
         ProgressiveScaling: bool, RandomBadgesBP: int, RandomBadgesFP: int, RandomPartnerFP: int, RandomStarpowerSP: int, RandomQuiz: bool, SkipQuiz: bool, QuizmoAlwaysAppears: bool, \
-        PartnersInDefaultLocations: bool, PartnersAlwaysUsable: bool, StartWithRandomPartners: bool, WriteSpoilerLog: bool, \
-        IncludeDojo: bool, BowsersCastleMode: int, ShortenCutscenes: bool = False, SkipEpilogue = False, RandomPartnersMin: int = None, RandomPartnersMax: int = None, StartWithPartners: StartWithPartners = None, \
-        StartingMap: int = 0x00010104, StartingMaxHP: int = 10, StartingMaxFP: int = 5, StartingMaxBP: int = 3, StartingStarPower: int = 0, StartingItem0: int = 0, StartingItem1: int = 0, StartingItem2: int = 0, StartingItem3: int = 0, \
+        PartnersInDefaultLocations: bool, PartnersAlwaysUsable: bool, StartWithRandomPartners: bool, WriteSpoilerLog: bool, RomanNumerals: bool, \
+        IncludeDojo: bool, BowsersCastleMode: int, ShortenCutscenes: bool = False, SkipEpilogue = False, RandomPartnersMin: int = None, RandomPartnersMax: int = None, StartWithPartners: StartWithPartners = None,
+        Box5ColorA: int = 0xEBE677FF, Box5ColorB: int = 0x8E5A25FF, RandomCoinColor: bool = False, CoinColor: int = 0, MarioSetting: int = 0, MarioSprite: int = 0, GoombarioSetting: int = 0, GoombarioSprite: int = 0,
+        KooperSetting: int = 0, KooperSprite: int = 0, BowSetting: int = 0, BowSprite: int = 0, BossesSetting: int = 0, NPCSetting: int = 0, StartingMap: int = 0x00010104,
+        StartingMaxHP: int = 10, StartingMaxFP: int = 5, StartingMaxBP: int = 3, StartingStarPower: int = 0, StartingItem0: int = 0, StartingItem1: int = 0, StartingItem2: int = 0, StartingItem3: int = 0, \
         StartingItem4: int = 0, StartingItem5: int = 0, StartingItem6: int = 0, StartingItem7: int = 0, StartingItem8: int = 0, StartingItem9: int = 0, StartingItemA: int = 0, StartingItemB: int = 0, \
         StartingItemC: int = 0, StartingItemD: int = 0, StartingItemE: int = 0, ItemScarcity: int = 0, StartingItemF: int = 0, StarWaySpiritsNeeded: int = 7,  SettingsString: str = None, \
-        FoliageItemHints = False, NoHealingItems =  False, StartWithRandomItems: bool = False, RandomItemsMin: int = 0, RandomItemsMax: int = 0, AddItemPouches = False, \
-        RandomChoice: bool = False, MysteryRandomPick: bool = False, ItemTrapMode: int = 0, AllowItemHints: bool = True):
+        FoliageItemHints = False, RandomText = False, NoHealingItems =  False, StartWithRandomItems: bool = False, RandomItemsMin: int = 0, RandomItemsMax: int = 0, AddItemPouches = False, \
+        RandomChoice: bool = False, MysteryRandomPick: bool = False, ItemTrapMode: int = 0, AllowItemHints: bool = True, WattSetting: int = 0, WattSprite: int = 0, SushieSetting: int = 0, SushieSprite: int = 0, \
+        ParakarrySetting: int = 0, ParakarrySprite: int = 0):
 
         self.SeedID = SeedID
         self.CreationDate = datetime.now()
@@ -68,10 +71,36 @@ class Seed:
         self.PartnersAlwaysUsable = PartnersAlwaysUsable
         self.StartWithRandomPartners = StartWithRandomPartners
         self.WriteSpoilerLog = WriteSpoilerLog
+        self.RomanNumerals = RomanNumerals
         self.IncludeDojo = IncludeDojo
         self.BowsersCastleMode = BowsersCastleMode
         self.ShortenCutscenes = ShortenCutscenes
         self.SkipEpilogue = SkipEpilogue
+
+        self.Box5ColorA = Box5ColorA
+        self.Box5ColorB = Box5ColorB
+        self.RandomCoinColor = RandomCoinColor
+        self.CoinColor = CoinColor
+
+        self.MarioSetting = MarioSetting
+        self.MarioSprite = MarioSprite
+        self.GoombarioSetting = GoombarioSetting
+        self.GoombarioSprite = GoombarioSprite
+        self.KooperSetting = KooperSetting
+        self.KooperSprite = KooperSprite
+        self.BowSetting = BowSetting
+        self.BowSprite = BowSprite
+
+        self.WattSetting = WattSetting
+        self.WattSprite = WattSprite
+        self.SushieSetting = SushieSetting
+        self.SushieSprite = SushieSprite
+        self.ParakarrySetting = ParakarrySetting
+        self.ParakarrySprite = ParakarrySprite
+
+        self.BossesSetting = BossesSetting
+        self.NPCSetting = NPCSetting
+
         
         self.StartingMap = StartingMap
         self.StartingMaxHP = StartingMaxHP
@@ -104,6 +133,7 @@ class Seed:
         self.ItemScarcity = ItemScarcity
         self.StarWaySpiritsNeeded = StarWaySpiritsNeeded
         self.FoliageItemHints = FoliageItemHints
+        self.RandomText = RandomText
         self.NoHealingItems = NoHealingItems
 
         self.RandomChoice = RandomChoice
@@ -125,33 +155,6 @@ class Seed:
         self.PlacementLogic = "NoGlitches"
         self.PeachCastleReturnPipe = True # Default
         self.ChallengeMode = False # Default
-
-        #Visuals are set to default, as they are set post-generation
-        self.Box5ColorA = 0xEBE677FF
-        self.Box5ColorB = 0x8E5A25FF
-        self.RandomCoinColor = False
-        self.CoinColor = 0        
-        self.RandomText = False        
-        self.RomanNumerals = False
-
-        self.MarioSetting = 0
-        self.MarioSprite = 0
-        self.GoombarioSetting = 0
-        self.GoombarioSprite = 0
-        self.KooperSetting = 0
-        self.KooperSprite = 0
-        self.BowSetting = 0
-        self.BowSprite = 0
-
-        self.WattSetting = 0
-        self.WattSprite = 0
-        self.SushieSetting = 0
-        self.SushieSprite = 0
-        self.ParakarrySetting = 0
-        self.ParakarrySprite = 0
-
-        self.BossesSetting = 0
-        self.NPCSetting = 0
 
     
     
