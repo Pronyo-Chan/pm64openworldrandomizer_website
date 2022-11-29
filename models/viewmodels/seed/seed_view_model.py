c328d842ec5ba3edb69e6330cbedc0ffd158922a
Pronyo <franktb93@gmail.com>
2022-11-29 13:17:28 -0500

    Add Difficulty options to view model

--- models/viewmodels/seed/seed_view_model.py
+++ models/viewmodels/seed/seed_view_model.py
@@ -1,4 +1,6 @@
+from models.viewmodels.seed.StatsAndGear import StatsAndGear
 from models.viewmodels.seed.cosmetics import Cosmetics
+from models.viewmodels.seed.general_difficulty import GeneralDifficulty
 from models.viewmodels.seed.partners import Partners
 from models.viewmodels.seed.items import Items
 from models.viewmodels.seed.gameplay import Gameplay
@@ -66,4 +68,51 @@ class SeedViewModel:
         ).__dict__
 
         self.Glitches = [g for g in glitches_names if seed_document[g] is True]
+
+        self.GeneralDifficulty = GeneralDifficulty(
+            progressive_scaling = seed_document["ProgressiveScaling"],
+            shuffle_chapter_difficulty = seed_document["ShuffleChapterDifficulty"],
+            double_damage = seed_document["DoubleDamage"],
+            quadruple_damage = seed_document["QuadrupleDamage"],
+            item_trap_mode = seed_document["ItemTrapMode"],
+            merlow_reward_pricing = seed_document["MerlowRewardPricing"],
+            cap_enemy_xp = seed_document["CapEnemyXP"],
+            no_xp = seed_document["NoXP"],
+            one_hit_ko = seed_document["OHKO"],
+            no_save_blocks = seed_document["NoSaveBlocks"],
+            no_heart_blocks = seed_document["NoHeartBlocks"],
+            no_healing_items = seed_document["NoHealingItems"],
+            random_consumable_mode = seed_document["RandomConsumableMode"],
+            item_quality = seed_document["ItemQuality"],
+            starway_spirits_needed = seed_document["StarWaySpiritsNeeded"]
+        ).__dict__
+
+        self.StatsAndGear = StatsAndGear(
+            starting_boots = seed_document["StartingBoots"],
+            starting_hammer = seed_document["StartingHammer"],
+            starting_coins = seed_document["StartingCoins"],
+            starting_max_hp = seed_document["StartingMaxHP"],
+            starting_max_bp = seed_document["StartingMaxBP"],
+            starting_max_fp = seed_document["StartingMaxFP"],
+            starting_star_power = seed_document["StartingStarPower"],
+            start_with_random_items = seed_document["StartWithRandomItems"],
+            random_items_min = seed_document["RandomItemsMin"],
+            random_items_max = seed_document["RandomItemsMax"],
+            starting_item_0 = seed_document["StartingItem0"],
+            starting_item_1 = seed_document["StartingItem1"],
+            starting_item_2 = seed_document["StartingItem2"],
+            starting_item_3 = seed_document["StartingItem3"],
+            starting_item_4 = seed_document["StartingItem4"],
+            starting_item_5 = seed_document["StartingItem5"],
+            starting_item_6 = seed_document["StartingItem6"],
+            starting_item_7 = seed_document["StartingItem7"],
+            starting_item_8 = seed_document["StartingItem8"],
+            starting_item_9 = seed_document["StartingItem9"],
+            starting_item_A = seed_document["StartingItemA"],
+            starting_item_B = seed_document["StartingItemB"],
+            starting_item_C = seed_document["StartingItemC"],
+            starting_item_D = seed_document["StartingItemD"],
+            starting_item_E = seed_document["StartingItemE"],
+            starting_item_F = seed_document["StartingItemF"]
+        ).__dict__
         
